6387aeb40f706cc49a9cdb4b913bc74d45545309
iamprincetj <tochukwunwanze5@gmail.com>
2023-11-25 23:27:36 +0100

    updated __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from api.auth import auth
 from api.views import views
 from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'the_secret_key_is_nothing'
+    os.environ['FLASK_ENV'] = os.getenv('FLASK_ENV')
 
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(views)
