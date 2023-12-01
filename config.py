30af5bac72c796fd732d401d4f4b2b84470eeab5
iamprincetj <tochukwunwanze5@gmail.com>
2023-12-01 02:21:35 +0100

    edit profile looked into

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config(object):
     MAIL_USE_TLS = os.getenv.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.getenv.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['tochukwunwanze5@gmail.com']
