30a93100df195ae824f5173f41640348edea9804
iamprincetj <tochukwunwanze5@gmail.com>
2023-12-07 20:00:37 +0100

    models.py updated

--- api/models.py
+++ api/models.py
@@ -16,10 +16,10 @@ ca = certifi.where()
 
 uri = f"mongodb+srv://{db_username}:{db_password}@alxportfolio1.rfv02wi.mongodb.net/JustIN_project?retryWrites=true&ssl=true&w=majority"
 
-#con = connect(host=uri, tlsCAFile=ca)
-#db = con.JustIN_project
-con = connect("tumblelog")
-db = con.tumblelog
+con = connect(host=uri, tlsCAFile=ca)
+db = con.JustIN_project
+#con = connect("tumblelog")
+#db = con.tumblelog
 
 
 
